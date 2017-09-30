f65ebd97c9213aa58c8a9bac59c742591c6c8033
inocop <inoue19851110@gmail.com>
2017-09-30 17:25:29 +0900

    create project

---
+++ src/ifc_web_viewer/Controllers/IfcViewerController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ifc_web_viewer.Models;
+using System.IO;
+
+namespace ifc_web_viewer.Controllers
+{
+    public class IfcViewerController : Controller
+    {
+        public IActionResult Index()
+        {
+            // get data directory files
+            string[] files = Directory.GetFiles( @"/opt/dotnet/src/data/", "*", SearchOption.AllDirectories);
+            ViewData["IfcFiles"] = files;
+         
+            return View();
+        }
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/ifc_web_viewer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ifc_web_viewer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
